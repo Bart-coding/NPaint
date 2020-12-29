2dc9284fb106b20ea8368f535dad4bf8c354e02f
Bart-coding <bartekpoczta54@gmail.com>
2020-12-29 18:08:29 +0100

    added class for state pattern

---
+++ State/PolygonState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPaint.State
+{
+    class PolygonState : MenuState
+    {
+        public override void MouseMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ State/SquareState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPaint.State
+{
+    class SquareState : MenuState
+    {
+        public override void MouseMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
