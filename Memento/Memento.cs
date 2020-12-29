073b2db05c6c7a0f21cc9c19a383844eeb0e97e3
Bart-coding <bartekpoczta54@gmail.com>
2020-12-29 21:23:32 +0100

    changes in memento pattern

--- Memento/Memento.cs
+++ Memento/Memento.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 
 namespace NPaint.Memento
 {
     class Memento
     {
+        Canvas State;
+
+        public Memento(Canvas State)
+        {
+            this.State = State;
+        }
+        public void SetState(Canvas State)
+        {
+            this.State = State;
+        }
+
+        public Canvas GetState()
+        {
+            return State;
+        }
     }
 }
