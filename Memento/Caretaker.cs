3e15c48538ade10193d20d792efeff6b67b1b205
Bart-coding <bartekpoczta54@gmail.com>
2020-12-29 19:20:36 +0100

    add memento pattern classes

---
+++ Memento/Caretaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPaint.Memento
+{
+    class Caretaker
+    {
+        private string FileName; //na sztywno; może final? 
+                                //ewentualnie listę eksportowaną/importowaną do pliku
+
+        public void AddMemento()
+        {
+
+        }
+    }
+}
---
+++ Memento/Originator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace NPaint.Memento
+{
+    class Originator
+    {
+        private Canvas state;
+        public void SetMemento (Memento m)
+        {
+            state = m.GetState();
+        }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(state);
+        }
+    }
+}
