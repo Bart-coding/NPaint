90a1d96e44ff02fa8f75592f7ac8de4e85719a78
Mateusz Żukowski <74957@student.pb.edu.pl>
2021-01-02 19:34:38 +0100

    implemented resize() method in NCircle, some changes in NSquare

--- Figures/NCircle.cs
+++ Figures/NCircle.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace NPaint.Figures
 {
     class NCircle : NEllipse
     {
         public NCircle() : base() {}
-        public override void MoveBy(Point point)
-        {
-            throw new NotImplementedException();
-        }
 
         public override void Resize(Point point)
         {
-            throw new NotImplementedException();
+            // obliczenie polozenia elipsy na osi XY
+            CenterPoint = MidPoint(point, startPoint);
+
+            // obliczenie długości promienia
+            double radius = Math.Abs(Math.Sqrt(Math.Pow(startPoint.X-point.X, 2) + Math.Pow(startPoint.Y - point.Y, 2)))/2;
+
+            // przypisanie wyliczonych wartosci do zmiennej (geometrii)
+            EllipseGeometry tmp = adaptedGeometry as EllipseGeometry;
+            //w momencie, gdy koło miałoby wchodzić na menu, wartości nie są aktualizowane, ale wygląda to lipnie i używa się tego lipnie - do poprawy, na razie pushuję takie bo działa. <-- najdłuższy komentarz na dzikim zachodzie
+            if(CenterPoint.Y - radius - ((MainWindow)System.Windows.Application.Current.MainWindow).BorderThicknessySlider.Value > 0)
+            {
+                tmp.Center = CenterPoint;
+                tmp.RadiusX = radius;
+                tmp.RadiusY = radius;
+            }
+
+            // przypisanie zmienionej geometrii do Path
+            adaptedPath.Data = adaptedGeometry;
         }
     }
 }
--- Figures/NSquare.cs
+++ Figures/NSquare.cs
@@ -16,46 +16,53 @@ namespace NPaint.Figures
             // zmienne pomocnicze
             double x, y, width, height, squarelength;
 
-            // sytuacja gdy rysujemy prawy dolny rog
-            if(point.X >= startPoint.X && point.Y >= startPoint.Y)
-            {
-                x = startPoint.X;
-                y = startPoint.Y;
-                width = point.X - x;
-                height = point.Y - y;
-                squarelength = Math.Min(width, height);
-            }
-            // sytuacja gdy rysujemy prawy gorny rog
-            else if (point.X > startPoint.X && point.Y < startPoint.Y)
-            {
-                x = startPoint.X;
-                y = point.Y;
-                width = point.X - x;
-                height = startPoint.Y - y;
-                squarelength = Math.Min(width, height);
-                y = startPoint.Y - squarelength;
-            }
-            // sytuacja gdy rysujemy lewy dolny rog
-            else if (point.X <= startPoint.X && point.Y >= startPoint.Y)
-            {
-                x = point.X;
-                y = startPoint.Y;
-                width = startPoint.X - x;
-                height = point.Y - y;
-                squarelength = Math.Min(width, height);
-                x = startPoint.X - squarelength;
-            }
-            // sytuacja gdy rysujemy lewy gorny rog
-            else
-            {
-                x = point.X;
-                y = point.Y;
-                width = startPoint.X - x;
-                height = startPoint.Y - y;
-                squarelength = Math.Min(width, height);
-                x = startPoint.X - squarelength;
-                y = startPoint.Y - squarelength;
-            }
+            width = Math.Abs(startPoint.X - point.X);
+            height = Math.Abs(startPoint.Y - point.Y);
+            squarelength = Math.Min(width, height);
+
+            x = Math.Min(startPoint.X, point.X); // do poprawy ale będzie śmigać :D
+            y = Math.Min(startPoint.Y, point.Y);
+
+            //// sytuacja gdy rysujemy prawy dolny rog
+            //if (point.X >= startPoint.X && point.Y >= startPoint.Y)
+            //{
+            //    x = startPoint.X;
+            //    y = startPoint.Y;
+            //    width = point.X - x;
+            //    height = point.Y - y;
+            //    squarelength = Math.Min(width, height);
+            //}
+            //// sytuacja gdy rysujemy prawy gorny rog
+            //else if (point.X > startPoint.X && point.Y < startPoint.Y)
+            //{
+            //    x = startPoint.X;
+            //    y = point.Y;
+            //    width = point.X - x;
+            //    height = startPoint.Y - y;
+            //    squarelength = Math.Min(width, height);
+            //    y = startPoint.Y - squarelength;
+            //}
+            //// sytuacja gdy rysujemy lewy dolny rog
+            //else if (point.X <= startPoint.X && point.Y >= startPoint.Y)
+            //{
+            //    x = point.X;
+            //    y = startPoint.Y;
+            //    width = startPoint.X - x;
+            //    height = point.Y - y;
+            //    squarelength = Math.Min(width, height);
+            //    x = startPoint.X - squarelength;
+            //}
+            //// sytuacja gdy rysujemy lewy gorny rog
+            //else
+            //{
+            //    x = point.X;
+            //    y = point.Y;
+            //    width = startPoint.X - x;
+            //    height = startPoint.Y - y;
+            //    squarelength = Math.Min(width, height);
+            //    x = startPoint.X - squarelength;
+            //    y = startPoint.Y - squarelength;
+            //}
 
             // przypisanie wyliczonych wartosci do zmiennej
             rect.X = x;
